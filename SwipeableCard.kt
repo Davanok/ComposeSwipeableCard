8b7b6c50e7894861423266eaf38cc8c70abc755d
Davanok <120126377+Davanok@users.noreply.github.com>
2024-03-02 19:22:36 +0300

    Add files via upload

---
+++ SwipeableCard.kt
@@ -0,0 +1,141 @@
+package com.example.dndinventory.ui.components
+
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.anchoredDraggable
+import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.example.dndinventory.OnStateChange
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+private enum class Anchors {
+    START,
+    CENTER,
+    END
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SwipeableCard(
+    modifier: Modifier = Modifier,
+    state: SwipeableCardState = rememberSwipeableCardState(),
+    key: () -> Int,
+    endContent: @Composable (RowScope.() -> Unit)? = null,
+    startContent: @Composable (RowScope.() -> Unit)? = null,
+    content: @Composable () -> Unit
+) {
+    val anchors = remember { mutableMapOf(Anchors.CENTER to 0f) }
+
+    val scope = rememberCoroutineScope()
+    val density = LocalDensity.current
+    val anchorState = remember {
+        AnchoredDraggableState(
+            initialValue = Anchors.CENTER,
+            positionalThreshold = { it * .5f },
+            animationSpec = spring(),
+            velocityThreshold = { with(density) { 100.dp.toPx() } },
+        ).apply {
+            updateAnchors(
+                DraggableAnchors {
+                    anchors.forEach { anchor ->
+                        anchor.key at anchor.value
+                    }
+                }
+            )
+        }
+    }
+    anchorState.OnStateChange {
+        if (it != Anchors.CENTER) state.currentAnchored = key()
+    }
+    if (state.currentAnchored != key() && anchorState.currentValue != Anchors.CENTER)
+        LaunchedEffect(key1 = Unit){
+            scope.launch {
+                anchorState.animateTo(Anchors.CENTER)
+            }
+        }
+    Box (
+        modifier = modifier
+    ) {
+        endContent?.let { eContent ->
+            Row (
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .onSizeChanged {
+                        scope.launch {
+                            anchorState.updateAnchors(
+                                DraggableAnchors {
+                                    anchors[Anchors.END] = -it.width.toFloat()
+                                    anchors.forEach { anchor ->
+                                        anchor.key at anchor.value
+                                    }
+                                }
+                            )
+                        }
+                    },
+            ) {
+                eContent()
+            }
+        }
+        startContent?.let { sContent ->
+            Row (
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .onSizeChanged {
+                        scope.launch {
+                            anchorState.updateAnchors(
+                                DraggableAnchors {
+                                    anchors[Anchors.START] = it.width.toFloat()
+                                    anchors.forEach { anchor ->
+                                        anchor.key at anchor.value
+                                    }
+                                }
+                            )
+                        }
+                    },
+            ) {
+                sContent()
+            }
+        }
+        Box (
+            modifier = Modifier
+                .offset {
+                    IntOffset(
+                        x = anchorState
+                            .requireOffset()
+                            .roundToInt(),
+                        y = 0
+                    )
+                }
+                .anchoredDraggable(anchorState, Orientation.Horizontal),
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun rememberSwipeableCardState() = remember { SwipeableCardState() }
+
+class SwipeableCardState {
+    var currentAnchored by mutableIntStateOf(-1)
+}
---
+++ SwipeableCardUsage.kt
@@ -0,0 +1,60 @@
+package com.example.dndinventory.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true)
+@Composable
+fun SwipeableCardUsage() {
+    val swipeAbleCardState = rememberSwipeableCardState()
+
+    LazyColumn (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(100) {
+            SwipeableCard(
+                state = swipeAbleCardState,
+                key = { it },
+                startContent = {
+                    FilledIconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+                    }
+                },
+                endContent = {
+                    FilledIconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Edit, contentDescription = "")
+                    }
+                    FilledIconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "")
+                    }
+                }
+            ) {
+                Card (
+                    modifier = Modifier.fillMaxWidth().height(80.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = "item: $it"
+                    )
+                }
+            }
+        }
+    }
+}
